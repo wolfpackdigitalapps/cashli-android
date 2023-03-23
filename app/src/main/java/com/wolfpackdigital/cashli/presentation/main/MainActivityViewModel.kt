ac5edee4d8c330dc399022381058746b9913bcdc
Valentin Brad <valentin.brad@wolfpack-digital.com>
2023-03-23 17:48:43 +0200

    Add delay to splash screen

--- app/src/main/java/com/wolfpackdigital/cashli/presentation/main/MainActivityViewModel.kt
+++ app/src/main/java/com/wolfpackdigital/cashli/presentation/main/MainActivityViewModel.kt
@@ -1,5 +1,22 @@
 package com.wolfpackdigital.cashli.presentation.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-class MainActivityViewModel : ViewModel()
+private const val SPLASH_SCREEN_DELAY = 2000L
+
+class MainActivityViewModel : ViewModel() {
+    private val _keepShowingSplash = MutableLiveData<Boolean>()
+    val keepShowingSplash: LiveData<Boolean> = _keepShowingSplash
+
+    init {
+        viewModelScope.launch {
+            delay(SPLASH_SCREEN_DELAY)
+            _keepShowingSplash.value = false
+        }
+    }
+}
