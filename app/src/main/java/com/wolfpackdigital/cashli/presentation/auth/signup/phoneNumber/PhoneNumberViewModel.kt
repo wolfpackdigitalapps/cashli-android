a517e6700d0876df258880b5dd3b33f32a9acc30
Handra Andrei <>
2023-03-29 13:16:37 +0300

    Added toolbar

--- app/src/main/java/com/wolfpackdigital/cashli/presentation/auth/signup/phoneNumber/PhoneNumberViewModel.kt
+++ app/src/main/java/com/wolfpackdigital/cashli/presentation/auth/signup/phoneNumber/PhoneNumberViewModel.kt
@@ -1,6 +1,20 @@
 package com.wolfpackdigital.cashli.presentation.auth.signup.phoneNumber
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.wolfpackdigital.cashli.R
+import com.wolfpackdigital.cashli.presentation.entities.Toolbar
 import com.wolfpackdigital.cashli.shared.base.BaseViewModel
 
 class PhoneNumberViewModel : BaseViewModel() {
+
+    private val _toolbar = MutableLiveData<Toolbar>()
+    val toolbar: LiveData<Toolbar> = _toolbar
+
+    init {
+        _toolbar.value = Toolbar(
+            titleLogoId = R.drawable.ic_logo_toolbar,
+            onBack = ::back
+        )
+    }
 }
