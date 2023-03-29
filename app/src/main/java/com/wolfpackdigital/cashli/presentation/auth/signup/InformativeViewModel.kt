ef93b980adf7757bf86c53fc0772d479b118bfc1
Handra Andrei <>
2023-03-29 14:12:27 +0300

    Added the necessary fonts and made changes, added the toolbar layout where it was needed

--- app/src/main/java/com/wolfpackdigital/cashli/presentation/auth/signup/InformativeViewModel.kt
+++ app/src/main/java/com/wolfpackdigital/cashli/presentation/auth/signup/InformativeViewModel.kt
@@ -1,9 +1,21 @@
 package com.wolfpackdigital.cashli.presentation.auth.signup
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.wolfpackdigital.cashli.R
+import com.wolfpackdigital.cashli.presentation.entities.Toolbar
 import com.wolfpackdigital.cashli.shared.base.BaseCommand
 import com.wolfpackdigital.cashli.shared.base.BaseViewModel
 
 class InformativeViewModel : BaseViewModel() {
+
+    private val _toolbar = MutableLiveData(
+        Toolbar(
+            titleLogoId = R.drawable.ic_logo_toolbar,
+            onBack = ::back
+        )
+    )
+    val toolbar: LiveData<Toolbar> = _toolbar
 
     fun signIn() {
         // TODO
--- app/src/main/java/com/wolfpackdigital/cashli/presentation/auth/signup/phoneNumber/PhoneNumberViewModel.kt
+++ app/src/main/java/com/wolfpackdigital/cashli/presentation/auth/signup/phoneNumber/PhoneNumberViewModel.kt
@@ -5,16 +5,18 @@ import androidx.lifecycle.MutableLiveData
 import com.wolfpackdigital.cashli.R
 import com.wolfpackdigital.cashli.presentation.entities.Toolbar
 import com.wolfpackdigital.cashli.shared.base.BaseViewModel
+import com.wolfpackdigital.cashli.shared.utils.Constants
 
-class PhoneNumberViewModel : BaseViewModel() {
+class PhoneNumberViewModel() : BaseViewModel() {
 
-    private val _toolbar = MutableLiveData<Toolbar>()
-    val toolbar: LiveData<Toolbar> = _toolbar
-
-    init {
-        _toolbar.value = Toolbar(
+    private val _toolbar = MutableLiveData(
+        Toolbar(
             titleLogoId = R.drawable.ic_logo_toolbar,
+            currentStep = Constants.STEP_1,
+            isStepCounterVisible = true,
             onBack = ::back
         )
-    }
+    )
+    val toolbar: LiveData<Toolbar> = _toolbar
+
 }
