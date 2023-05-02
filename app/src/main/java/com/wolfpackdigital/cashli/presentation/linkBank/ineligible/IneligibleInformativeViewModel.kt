daa1d7018cd9d653d506a0ccfec370c18a9e8904
Handra Andrei <>
2023-05-02 18:05:53 +0300

    Almost implemented design

--- app/src/main/java/com/wolfpackdigital/cashli/presentation/linkBank/ineligible/IneligibleInformativeViewModel.kt
+++ app/src/main/java/com/wolfpackdigital/cashli/presentation/linkBank/ineligible/IneligibleInformativeViewModel.kt
@@ -1,6 +1,16 @@
 package com.wolfpackdigital.cashli.presentation.linkBank.ineligible
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.wolfpackdigital.cashli.presentation.entities.Toolbar
+import com.wolfpackdigital.cashli.shared.base.BaseCommand
 import com.wolfpackdigital.cashli.shared.base.BaseViewModel
 
 class IneligibleInformativeViewModel : BaseViewModel() {
+
+    private val _toolbar = MutableLiveData(
+        Toolbar(onBack = ::back)
+    )
+    val toolbar: LiveData<Toolbar> = _toolbar
+
 }
