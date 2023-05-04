90f27329e6cfc52f6f3c09f8f77d9f91894b9ec7
Handra Andrei <>
2023-05-04 19:25:43 +0300

    Integrated with CAS-53

--- app/src/main/java/com/wolfpackdigital/cashli/presentation/linkBank/ineligible/IneligibleInformativeViewModel.kt
+++ app/src/main/java/com/wolfpackdigital/cashli/presentation/linkBank/ineligible/IneligibleInformativeViewModel.kt
@@ -1,7 +1,6 @@
 package com.wolfpackdigital.cashli.presentation.linkBank.ineligible
 
-import android.text.SpannableString
-import android.text.style.ImageSpan
+
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.wolfpackdigital.cashli.R
@@ -12,8 +11,12 @@ import com.wolfpackdigital.cashli.shared.base.BaseViewModel
 class IneligibleInformativeViewModel : BaseViewModel() {
 
     private val _toolbar = MutableLiveData(
-        Toolbar(onBack = ::back)
+        Toolbar(onBack = ::onBackPressed)
     )
     val toolbar: LiveData<Toolbar> = _toolbar
+
+    private fun onBackPressed() {
+        _baseCmd.value = BaseCommand.GoBackTo(R.id.homeFragment)
+    }
 
 }
