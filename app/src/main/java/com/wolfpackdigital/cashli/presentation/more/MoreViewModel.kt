17b2e722258fa427a9f8916e12f2376dc7a50b3c
Handra Andrei <>
2023-05-05 18:01:18 +0300

    Started menu screen

--- app/src/main/java/com/wolfpackdigital/cashli/presentation/more/MoreViewModel.kt
+++ app/src/main/java/com/wolfpackdigital/cashli/presentation/more/MoreViewModel.kt
@@ -3,11 +3,15 @@ package com.wolfpackdigital.cashli.presentation.more
 import androidx.lifecycle.LiveData
 import com.wolfpackdigital.cashli.shared.base.BaseViewModel
 import com.wolfpackdigital.cashli.shared.utils.LiveEvent
+import com.wolfpackdigital.cashli.shared.utils.persistence.PersistenceService
 
-class MoreViewModel : BaseViewModel() {
+class MoreViewModel : BaseViewModel(), PersistenceService {
 
     private val _cmd = LiveEvent<Command>()
     val cmd: LiveData<Command> = _cmd
+
+    val firstName = userProfile?.firstName
+    val lastName = userProfile?.lastName
 
     sealed class Command
 }
