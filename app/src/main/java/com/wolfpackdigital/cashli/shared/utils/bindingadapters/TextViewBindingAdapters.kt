c02b11a2b20dc894f3d4fa1a5bac0258341ba38a
Mihai Dornea <mihai.dornea@wolfpack-digital.com>
2023-05-06 16:54:13 +0300

    chore: resolve merge conflicts

--- app/src/main/java/com/wolfpackdigital/cashli/shared/utils/bindingadapters/TextViewBindingAdapters.kt
+++ app/src/main/java/com/wolfpackdigital/cashli/shared/utils/bindingadapters/TextViewBindingAdapters.kt
@@ -2,14 +2,25 @@ package com.wolfpackdigital.cashli.shared.utils.bindingadapters
 
 import android.graphics.Color
 import android.text.Annotation
+import android.text.Spannable
 import android.text.SpannableString
 import android.text.SpannedString
 import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.text.style.ImageSpan
+import android.view.Gravity
+import android.view.View
 import android.widget.TextView
+import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.databinding.BindingAdapter
+import androidx.lifecycle.findViewTreeLifecycleOwner
+import com.skydoves.balloon.Balloon
+import com.skydoves.balloon.BalloonAnimation
+import com.skydoves.balloon.BalloonSizeSpec
 import com.wolfpackdigital.cashli.R
 import com.wolfpackdigital.cashli.presentation.entities.TextSpanAction
+import com.wolfpackdigital.cashli.shared.utils.Constants
 import com.wolfpackdigital.cashli.shared.utils.CustomClickSpan
 import com.wolfpackdigital.cashli.shared.utils.extensions.getStringFromResourceOrText
 
@@ -58,4 +69,49 @@ fun TextView.setTextSpanByAction(actions: List<TextSpanAction>, @StringRes textW
     text = termsCopy
     movementMethod = LinkMovementMethod.getInstance()
     highlightColor = Color.TRANSPARENT
+}
+
+
+@BindingAdapter(
+    value = ["requirement_text", "tooltip_text", "requirement_image"],
+    requireAll = true
+)
+fun TextView.setCustomTextAndBalloon(
+    text: String,
+    tooltipText: String,
+    @DrawableRes image: Int
+) {
+    val string = SpannableString(text + Constants.DOUBLE_SPACE)
+    string.setSpan(
+        ImageSpan(context, image), string.length - 1, string.length,
+        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+    )
+    val clickableSpan: ClickableSpan = object : ClickableSpan() {
+        override fun onClick(widget: View) {
+            Balloon.Builder(context)
+                .setIsVisibleArrow(false)
+                .setWidth(BalloonSizeSpec.WRAP)
+                .setHeight(BalloonSizeSpec.WRAP)
+                .setText(tooltipText)
+                .setPaddingResource(R.dimen.dimen_16dp)
+                .setPaddingRightResource(R.dimen.dimen_32dp)
+                .setCornerRadiusResource(R.dimen.dimen_8dp)
+                .setTextColorResource(R.color.colorGrayB6)
+                .setTextSizeResource(R.dimen.size_12sp)
+                .setTextGravity(Gravity.START)
+                .setBalloonAnimation(BalloonAnimation.FADE)
+                .setBackgroundColorResource(R.color.colorWhiteF9)
+                .setLifecycleOwner(findViewTreeLifecycleOwner())
+                .build()
+                .showAlignBottom(this@setCustomTextAndBalloon)
+        }
+    }
+    string.setSpan(
+        clickableSpan,
+        string.length - 1,
+        string.length,
+        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+    )
+    movementMethod = LinkMovementMethod.getInstance()
+    setText(string)
 }
--- app/src/main/java/com/wolfpackdigital/cashli/shared/utils/bindingadapters/ViewBindingAdapters.kt
+++ app/src/main/java/com/wolfpackdigital/cashli/shared/utils/bindingadapters/ViewBindingAdapters.kt
@@ -2,25 +2,8 @@ package com.wolfpackdigital.cashli.shared.utils.bindingadapters
 
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
-import android.graphics.Color
-import android.text.Annotation
-import android.text.Spannable
-import android.text.SpannableString
-import android.text.SpannedString
-import android.text.method.LinkMovementMethod
-import android.text.style.ClickableSpan
-import android.text.style.ImageSpan
-import android.view.Gravity
 import android.view.View
 import androidx.databinding.BindingAdapter
-import androidx.lifecycle.findViewTreeLifecycleOwner
-import com.google.android.material.button.MaterialButton
-import com.skydoves.balloon.Balloon
-import com.skydoves.balloon.BalloonAnimation
-import com.skydoves.balloon.BalloonSizeSpec
-import com.wolfpackdigital.cashli.R
-import com.wolfpackdigital.cashli.presentation.entities.TextSpanAction
-import com.wolfpackdigital.cashli.shared.utils.Constants
 import com.wolfpackdigital.cashli.shared.utils.Constants.ALPHA_0
 import com.wolfpackdigital.cashli.shared.utils.Constants.ALPHA_1
 import com.wolfpackdigital.cashli.shared.utils.Constants.DEBOUNCE_INTERVAL_MILLIS_300
@@ -69,68 +52,4 @@ fun View.setFadeVisibility(viewState: Int?) {
             animate().setDuration(FADE_ANIM_DURATION_200).alpha(ALPHA_0).setListener(endListener)
         }
     }
-}
-
-@BindingAdapter(
-    value = ["requirement_text", "tooltip_text", "requirement_image"],
-    requireAll = true
-)
-fun TextView.setCustomTextAndBalloon(
-    text: String,
-    tooltipText: String,
-    @DrawableRes image: Int
-) {
-    val string = SpannableString(text + Constants.DOUBLE_SPACE)
-    string.setSpan(
-        ImageSpan(context, image), string.length - 1, string.length,
-        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-    )
-    val clickableSpan: ClickableSpan = object : ClickableSpan() {
-        override fun onClick(widget: View) {
-            Balloon.Builder(context)
-                .setIsVisibleArrow(false)
-                .setWidth(BalloonSizeSpec.WRAP)
-                .setHeight(BalloonSizeSpec.WRAP)
-                .setText(tooltipText)
-                .setPaddingResource(R.dimen.dimen_16dp)
-                .setPaddingRightResource(R.dimen.dimen_32dp)
-                .setCornerRadiusResource(R.dimen.dimen_8dp)
-                .setTextColorResource(R.color.colorGrayB6)
-                .setTextSizeResource(R.dimen.size_12sp)
-                .setTextGravity(Gravity.START)
-                .setBalloonAnimation(BalloonAnimation.FADE)
-                .setBackgroundColorResource(R.color.colorWhiteF9)
-                .setLifecycleOwner(findViewTreeLifecycleOwner())
-                .build()
-                .showAlignBottom(this@setCustomTextAndBalloon)
-        }
-    }
-    string.setSpan(
-        clickableSpan,
-        string.length - 1,
-        string.length,
-        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-    )
-    movementMethod = LinkMovementMethod.getInstance()
-    setText(string)
-}
-
-@BindingAdapter("showTooltip")
-fun TextView.showTooltip(@StringRes text: Int) {
-    Balloon.Builder(context)
-        .setIsVisibleArrow(false)
-        .setWidth(BalloonSizeSpec.WRAP)
-        .setHeight(BalloonSizeSpec.WRAP)
-        .setText(context.getString(text))
-        .setPaddingResource(R.dimen.dimen_16dp)
-        .setPaddingRightResource(R.dimen.dimen_32dp)
-        .setCornerRadiusResource(R.dimen.dimen_8dp)
-        .setTextColorResource(R.color.colorGrayB6)
-        .setTextSizeResource(R.dimen.size_12sp)
-        .setTextGravity(Gravity.START)
-        .setBalloonAnimation(BalloonAnimation.FADE)
-        .setBackgroundColorResource(R.color.colorWhiteF9)
-        .setLifecycleOwner(findViewTreeLifecycleOwner())
-        .build()
-        .showAlignBottom(this)
 }
