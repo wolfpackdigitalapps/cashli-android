c743b6d7e057de8037e5e7d3204e6623b0d75f54
Valentin Brad <valentin.brad@wolfpack-digital.com>
2023-05-10 13:11:34 +0300

    kotlin

--- app/src/main/java/com/wolfpackdigital/cashli/shared/base/Result.kt
+++ app/src/main/java/com/wolfpackdigital/cashli/shared/base/Result.kt
@@ -8,8 +8,6 @@ import kotlinx.parcelize.Parcelize
  * A generic class that holds a value with its loading status.
  * @param <T>
  */
-
-@Keep
 sealed class Result<out R> {
 
     data class Success<out T>(val data: T) : Result<T>()
--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -7,7 +7,7 @@ object Versions {
     const val TARGET_SDK = 33
     const val MIN_SDK = 26
 
-    const val KOTLIN = "1.8.10"
+    const val KOTLIN = "1.8.21"
     const val BUILD_GRADLE = "8.0.1"
 
     const val ANDROID_NAVIGATION = "2.5.3"
