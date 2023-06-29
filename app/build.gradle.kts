bdad55dc73f9dec7636bea0dc0b92741e8d6e649
Valentin Brad <valentin.brad@wolfpack-digital.com>
2023-06-29 11:28:18 +0300

    New version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,7 +65,7 @@ android {
                 "BASE_URL",
                 "\"https://api.cashli.io/api/\""
             )
-            versionCode = 1
+            versionCode = 3
             versionName = "1.0.0"
         }
     }
